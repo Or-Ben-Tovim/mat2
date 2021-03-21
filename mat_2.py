c78c387aa6a8a9be0531b8aca40ad5c7c20ca4df
Or-Ben-Tovim <orbentovim11200@gmail.com>
2021-03-21 07:47:36 +0200

    first

---
+++ mat_2.py
@@ -0,0 +1,24 @@
+def revword(word:str) -> str:
+    result=word[::-1]
+    result=result.lower()
+    return(result)
+
+def countword()->int:
+    file=open("text.txt")
+    counter=0
+    index=0
+    for i in file:
+        if index==0:
+            word=i.lower()
+            index=1
+            word=word.rstrip()
+        line=i.split()
+        for j in line:
+            reWord=revword(j)
+            if(reWord==word):
+                counter=counter+1
+    return counter 
+
+print(countword()+1)
+           
+
